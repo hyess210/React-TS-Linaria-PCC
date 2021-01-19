03eb46dff811a3edc79edc9130444aeeaffa23a9
hyeseon <hyeseon8562@gmail.com>
2021-01-19 22:14:19 +0900

    clear git cache

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'components/App';
+import { css } from '@linaria/core'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
